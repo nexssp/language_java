2a570bd894b8cef3b08cc60e199db7b0e46ea140
Marcin Polak <mapoart@gmail.com>
2020-09-29 11:55:41 +0200

    Removed @nexssp/os and take from central cli src

--- java.linux.nexss.config.js
+++ java.linux.nexss.config.js
@@ -1,6 +1,8 @@
 let languageConfig = Object.assign({}, require(`./java.win32.nexss.config`));
 
-const os = require("@nexssp/os");
+// const os = require("@nexssp/os")
+// Load os from Nexss CLI path (it can be changed if needed)
+const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
 const sudo = os.sudo();
 
 const distName = os.name();
@@ -23,6 +25,9 @@ languageConfig.compilers = {
 };
 
 switch (distName) {
+  case os.distros.DEBIAN:
+    languageConfig.compilers.java8.install = "apt install -y openjdk-11-jdk";
+    break;
   case os.distros.CENTOS:
   case os.distros.FEDORA:
     languageConfig.compilers.java8.install = os.replacePMByDistro(
