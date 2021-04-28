dc036940734288a07febbbdb42617d1dfbd3fa3a
Marcin Polak <mapoart@gmail.com>
2021-04-28 12:51:52 +0200

    New Template Test.java

---
+++ templates/Test.java
@@ -0,0 +1,7 @@
+import org.json.JSONObject;
+public class Test {
+    public static void main(String args[]){
+       JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
+       System.out.println(jo.toString());
+    }
+}
